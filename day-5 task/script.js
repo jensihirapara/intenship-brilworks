e773995d0a8ce629e5ca9680aae16fd79ed41fac
JENSIHIRAPARA <jensihirapara3@gmail.com>
2025-03-27 14:58:49 +0530

    add more css

--- day-5 task/script.js
+++ day-5 task/script.js
@@ -30,8 +30,18 @@ function closeModal() {
 }
 
 function saveRecord() {
+
   const name = document.getElementById("nameInput").value;
   const email = document.getElementById("emailInput").value;
+  
+  const data = localStorage.getItem("formData");
+
+  const jsonDataList = data ? JSON.parse(data):[];
+  const newObj = {name,email};
+  jsonDataList.push(newObj)
+
+
+ 
   const table = document
     .getElementById("data-table")
     .getElementsByTagName("tbody")[0];
@@ -55,11 +65,60 @@ function saveRecord() {
               <button onclick="deleteRecord(this)">Delete</button>
           `;
   }
-
+  localStorage.setItem("formData",JSON.stringify(jsonDataList));
   closeModal();
 }
 
-function deleteRecord(button) {
-  const row = button.parentNode.parentNode;
-  row.parentNode.removeChild(row);
+function deleteRecord(key) {
+  console.log(key);
+  // const row = button.parentNode.parentNode;
+  ///row.parentNode.removeChild(row);
+
+  
+}
+
+function loadSetUpData(button) {
+  const data = localStorage.getItem("formData");
+  console.log("Data00",data);
+  const jsonData= data?JSON.parse(data):[];
+  console.log("jsonData",jsonData);
+
+for(var i = 0;i <=jsonData.length; i++){
+  console.log(jsonData[i], "Value of i", i)
+
+  const name = jsonData[i].name;
+  const email = jsonData[i].email;
+  const table = document
+  .getElementById("data-table")
+  .getElementsByTagName("tbody")[0];
+
+if (editingIndex !== null) {
+ 
+  const row = table.rows[editingIndex];
+  row.cells[0].innerText = name;
+  row.cells[1].innerText = email;
+} else {
+ 
+  const newRow = table.insertRow();
+  newRow.insertCell(0).innerText = name;
+  newRow.insertCell(1).innerText = email;
+
+  const actionsCell = newRow.insertCell(2);
+  actionsCell.innerHTML = `
+            <button onclick="openModal(${
+              table.rows.length - 1
+            })">Edit</button>
+            <button onclick="deleteRecord(${i})">Delete</button>
+        `;
+}
+}
+
+
+ 
+}
+this.loadSetUpData();
+
+function deleteRow(key){
+  console.log(key)
+  // localStorage.removeItem(key)
 }
